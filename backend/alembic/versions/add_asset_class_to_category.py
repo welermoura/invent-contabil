60fc83cbfda8e38f2a59a0a767dde68f7869ab4c
Weler Moura <welerMS@comolatti.com.br>
2026-04-14 17:35:07 -0300

    fix(db): correct alembic down_revision to 79b15643ec08

--- backend/alembic/versions/add_asset_class_to_category.py
+++ backend/alembic/versions/add_asset_class_to_category.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '52a1b3c4d5e6'
-down_revision = '888888888888'
+down_revision = '79b15643ec08'
 branch_labels = None
 depends_on = None
 
